1817a751f586571eb08b061d3b695f7659404079
Ilya Boyko <furagi.usagi@gmail.com>
2025-02-26 22:06:59 +0400

    added photo and converting the cv to images

--- convert.sh
+++ convert.sh
@@ -1,2 +1,3 @@
 docker run --rm -v "$(pwd)":/docs sphinxdoc/sphinx-latexpdf latexmk -outdir=/docs -pdf /docs/cv.tex
-
+docker run --rm -v "$(pwd)":/imgs dpokidov/imagemagick cv.pdf[0] -background white -flatten -quality 90 /imgs/cv-1.png
+docker run --rm -v "$(pwd)":/imgs dpokidov/imagemagick cv.pdf[1] -background white -flatten -quality 90 /imgs/cv-2.png
